ad0dcba636c4203b4f92115a8bf06003def69385
Elena <pavlen84@hotmail.com>
2016-04-12 11:35:03 +0200

    modif gitignore

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -1,7 +1,7 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
-<?php $this->start('main_content') ?>
+<?php $this->start('principal') ?>
 	<h2>Let's code.</h2>
 	<p>Vous avez atteint la page d'accueil. Bravo.</p>
 	<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
-<?php $this->stop('main_content') ?>
+<?php $this->stop('principal') ?>
