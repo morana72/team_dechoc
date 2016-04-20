5a0d095eb39acfd7776d362a0daa2337d34ba7a2
Lovincy JOSEPH <lovincy.joseph@gmail.com>
2016-04-20 12:48:22 +0200

    ajouts Lovincy

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -71,10 +71,15 @@
 </footer>
 <!--_______________________________ Fin de la partie footer _____________________________-->
 
-<script type="text/javascript" src="<?= $this->assetUrl('js/map_musee.js') ?>"></script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcMX7HOEZhjNWNoYpaRPopYsEv42hl1kk&callback=initMap"></script>
-<script src="https://code.jquery.com/jquery-1.12.1.min.js" integrity="sha256-I1nTg78tSrZev3kjvfdM5A5Ak/blglGzlaZANLPDl3I=" crossorigin="anonymous"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=	AIzaSyA5-6P6fElf8xdI8v2pwSt9NokAAIHBFXs
+        &libraries=visualization&callback=initMap"></script>
+<script src="https://code.jquery.com/jquery-1.12.1.min.js"   integrity="sha256-I1nTg78tSrZev3kjvfdM5A5Ak/blglGzlaZANLPDl3I="   crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<!-- script pour activer google map -->
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="<?= $this->assetUrl('js/script_musees.js') ?>"></script>
 
 </body>
 </html>
